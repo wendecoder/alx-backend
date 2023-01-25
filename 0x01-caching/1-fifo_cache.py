d4762fea98615755da8716d19403e8f7c36382f9
wendecoder <wenonezra258@gmail.com>
2023-01-25 23:27:37 +0300

    added all the tasks

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+A script that defines a class that uses
+A FIFO method
+"""
+BaseCaching = __import__('base_catching').BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """ FIFOCache
+    """
+    def __init__(self):
+        super().__init__()
+
+    def put(self, key, item):
+        """ assigns the new item to the dictionary
+        """
+        if not (key is None or item is None):
+            self.cache_data.update({key: item})
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                first_in = list(self.cache_data.keys())[0]
+                del self.cache_data[first_in]
+                print(f'DISCARD: {first_in}')
+
+    def get(self, key):
+        """ returns the value in self.cache_data linked to key
+        """
+        if key is None or not (key in self.cache_data):
+            return None
+        return self.cache_data[key]
